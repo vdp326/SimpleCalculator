108c5b34e2d741d8c4849acfba19e6d6f7472002
Vu Pham <vupham326@gmail.com>
2019-02-02 00:26:34 -0800

    simple calculator in C#

---
+++ SimpleCalculator/Default.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SimpleCalculator.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+     <style type="text/css">
+          .auto-style1 {
+               font-family: Arial, Helvetica, sans-serif;
+          }
+     </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+         <h1>Simple Calculator</h1>
+         <p>
+              &nbsp;</p>
+         <p>
+              <span class="auto-style1">First Value</span>:
+              <asp:TextBox ID="val1TextBox" runat="server"></asp:TextBox>
+         </p>
+         <p>
+              <span class="auto-style1">Second Value</span>:
+              <asp:TextBox ID="val2TextBox" runat="server" OnTextChanged="TextBox2_TextChanged"></asp:TextBox>
+         </p>
+         <p>
+              &nbsp;</p>
+    
+    </div>
+         <asp:Button ID="additionButton" runat="server" OnClick="additionButton_Click" Text="+" Width="55px" />
+&nbsp;&nbsp;
+         <asp:Button ID="subtractionButton" runat="server" OnClick="subtractionButton_Click" Text="-" Width="55px" />
+&nbsp;&nbsp;
+         <asp:Button ID="multiplyButton" runat="server" OnClick="multiplyButton_Click" Text="*" Width="55px" />
+&nbsp;&nbsp;
+         <asp:Button ID="divideButton" runat="server" OnClick="divideButton_Click" Text="/" Width="55px" />
+         <br />
+         <br />
+         <asp:Label ID="resultLabel" runat="server" BackColor="#99CCFF" style="font-weight: 700; background-color: #3399FF"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ SimpleCalculator/Default.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SimpleCalculator
+{
+     public partial class Default : System.Web.UI.Page
+     {
+          protected void Page_Load(object sender, EventArgs e)
+          {
+
+          }
+
+          protected void TextBox2_TextChanged(object sender, EventArgs e)
+          {
+
+          }
+
+          protected void additionButton_Click(object sender, EventArgs e)
+          {
+               string firstVal = val1TextBox.Text;
+               string secondVal = val2TextBox.Text;
+               double result = double.Parse(firstVal) + double.Parse(secondVal);
+
+               resultLabel.Text = result.ToString();
+
+          }
+
+          protected void subtractionButton_Click(object sender, EventArgs e)
+          {
+               string firstVal = val1TextBox.Text;
+               string secondVal = val2TextBox.Text;
+               double result = double.Parse(firstVal) - double.Parse(secondVal);
+
+               resultLabel.Text = result.ToString();
+
+          }
+
+          protected void multiplyButton_Click(object sender, EventArgs e)
+          {
+               string firstVal = val1TextBox.Text;
+               string secondVal = val2TextBox.Text;
+               double result = double.Parse(firstVal) * double.Parse(secondVal);
+
+               resultLabel.Text = result.ToString();
+
+          }
+
+          protected void divideButton_Click(object sender, EventArgs e)
+          {
+               string firstVal = val1TextBox.Text;
+               string secondVal = val2TextBox.Text;
+               double result = double.Parse(firstVal) / double.Parse(secondVal);
+
+               resultLabel.Text = result.ToString();
+
+          }
+     }
+}
